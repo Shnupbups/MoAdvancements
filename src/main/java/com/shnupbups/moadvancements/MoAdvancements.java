c3356edf878a799632249c0c608c33937142d838
Shnupbups <shnupbups@gmail.com>
2020-12-31 03:57:08 +1100

    init

--- src/main/java/com/shnupbups/moadvancements/MoAdvancements.java
+++ src/main/java/com/shnupbups/moadvancements/MoAdvancements.java
@@ -1,11 +1,11 @@
-package com.shnupbups.modid;
+package com.shnupbups.moadvancements;
 
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
 
-public class ModId implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class MoAdvancements implements ModInitializer {
+	public static final String MOD_ID = "moadvancements";
 	
 	@Override
 	public void onInitialize() {
